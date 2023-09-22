42960aa6f2c8aa38fc29623a7cbae2f3b6ca803f
Dylan Timmons <dylanstimmons@icloud.com>
2023-09-22 16:48:25 -0600

    Adds new, create and bulk params

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -10,6 +10,24 @@ class BulkDiscountsController < ApplicationController
     end
 
     def new
-        @merchant = Merchant.find(params[:id])
+        @merchant = Merchant.find(params[:merchant_id])
+        @discount = BulkDiscount.new
     end
+
+    def create
+      @merchant = Merchant.find(params[:merchant_id])
+      @discount = @merchant.bulk_discounts.new(bulk_discount_params)
+      if params[:bulk_discount][:quantity].blank? || params[:bulk_discount][:discount].blank?
+          flash[:notice] = "One or more of the required fields is empty. Bulk Discount cannot be created"
+          redirect_to new_merchant_bulk_discount_path(@merchant.id)
+      elsif @discount.save
+          flash[:notice] = "Discount##{@discount.id} has been Created!"
+          redirect_to merchant_bulk_discounts_path(@merchant.id)
+      end
+    end
+
+    private
+     def bulk_discount_params
+        params.require(:bulk_discount).permit(:quantity, :discount)
+     end
 end
