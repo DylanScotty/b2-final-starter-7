c375e4aca87b9fea974b1c394e855925d8ef51e1
Dylan Timmons <dylanstimmons@icloud.com>
2023-09-23 08:34:45 -0600

    add test for show page

---
+++ spec/features/bulk_discounts/show_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe 'Merchant Bulk Discounts Show' do
+  before :each do
+    @merchant = Merchant.create!(name: 'Computer Parts')
+
+    @bulk_discount = BulkDiscount.create!(quantity: 10, discount: 20, merchant_id: @merchant.id)
+
+    visit merchant_bulk_discount_path(@merchant, @bulk_discount)
+  end
+
+   describe 'Show page' do
+     it "shows the discount's quantity and percentage" do 
+        save_and_open_page
+      expect(page).to have_content("Discount ##{@bulk_discount.id}")
+      expect(page).to have_content("Quantity threshold: #{@bulk_discount.quantity} items")
+      expect(page).to have_content("Discount percentage: #{@bulk_discount.discount}%")
+     end
+   end
+end
